3f5ee3a4b12b4b43926941a35f6c74f0f032ac9c
Ibtissam <bricohouse.stri@gmail.com>
2019-11-13 10:13:22 +0100

    Hello!

---
+++ app/src/main/java/com/example/bricohouse/activity/Inscription_client.java
@@ -0,0 +1,71 @@
+package com.example.bricohouse.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.example.bricohouse.R;
+import com.example.bricohouse.bean.Client;
+import com.example.bricohouse.bean.User;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inscription_client extends AppCompatActivity {
+    Client client = new Client();
+    Button valider;
+    DatabaseReference reff;
+    EditText name;
+    EditText last_name;
+    EditText phone;
+    EditText mail;
+    EditText adress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_incription_client);
+        //Toast.makeText(Inscription_client.this, "wwwwwwwwwwwwajisjdjsidjisjdi", Toast.LENGTH_SHORT).show();
+
+
+        name= (EditText) findViewById(R.id.name);
+        last_name= (EditText) findViewById(R.id.last_name);
+        phone=(EditText) findViewById(R.id.phone);
+        mail=(EditText) findViewById(R.id.mail);
+        adress=(EditText) findViewById(R.id.adress);
+            valider=(Button) findViewById(R.id.valider);
+            reff= FirebaseDatabase.getInstance().getReference().child("bean.User");
+
+
+
+
+            //public void save(User utilisateur){
+            // utilisateur.setLogin(utilisateur.getLogin());
+        }
+
+
+    public void save(View view) {
+        String prenom= name.getText().toString();
+        String nom= last_name.getText().toString();
+        String tel=phone.getText().toString();
+        String email=mail.getText().toString();
+        String adr=adress.getText().toString();
+        client.setNom(nom);
+        client.setPrenom(prenom);
+        client.setPhone(tel);
+        client.setEmail(email);
+        client.setAdresseComplement(adr);
+        reff.push().setValue(client);
+    }
+
+    }
+
