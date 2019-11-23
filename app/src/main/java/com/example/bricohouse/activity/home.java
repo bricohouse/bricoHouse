b49f5d6da6272f89e9a5774eb551a082775c97aa
oumaima <bricohouse.stri@gmail.com>
2019-11-23 15:12:22 +0000

    hihooooo

--- app/src/main/java/com/example/bricohouse/activity/home.java
+++ app/src/main/java/com/example/bricohouse/activity/home.java
@@ -2,7 +2,9 @@ package com.example.bricohouse.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.bricohouse.R;
 
@@ -12,5 +14,10 @@ public class home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void signin(View view) {
+        Intent intent=new Intent(home.this,login_activity.class);
+        startActivity(intent);
     }
 }
